a81d060d2b23f9f441df479b9292b64c6e592fd5
François Lenne <francois.lenne@hotmail.fr>
2025-01-19 12:19:40 +0100

    dockerize the audio retrieving and begin with whisper

--- src/retrieve-audio/retrieve-audio.py
+++ src/retrieve-audio/retrieve-audio.py
@@ -1,34 +1,45 @@
 import yt_dlp
 import os
+import argparse
 
-def download_webtv_audio(url, output_path):
-    try:
-        # Options de configuration pour l'audio uniquement
-        ydl_opts = {
-            'format': 'bestaudio/best',  # Meilleure qualité audio disponible
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-            'outtmpl': os.path.join(output_path, '%(title)s.%(ext)s'),
-            'quiet': False,
-            'no_warnings': False
-        }
+class WebTVAudioDownloader:
+    def __init__(self, url, output_path):
+        self.url = url
+        self.output_path = output_path
+
+    def download_webtv_audio(self):
+        try:
+            # Options de configuration pour l'audio uniquement
+            ydl_opts = {
+                'format': 'bestaudio/best',  # Meilleure qualité audio disponible
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+                'outtmpl': os.path.join(self.output_path, '%(title)s.%(ext)s'),
+                'quiet': False,
+                'no_warnings': False
+            }
+            
+            # Téléchargement de l'audio
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([self.url])
+                
+            print(f"Téléchargement audio terminé dans : {self.output_path}")
+                
+        except Exception as e:
+            print(f"Une erreur est survenue : {str(e)}")
+            return False
         
-        # Téléchargement de l'audio
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-            
-        print(f"Téléchargement audio terminé dans : {output_path}")
-            
-    except Exception as e:
-        print(f"Une erreur est survenue : {str(e)}")
-        return False
-    
-    return True
+        return True
 
-# Utilisation
-url = "https://webtv.hautsdefrance.fr/Seance-pleniere-du-1er-fevrier-2024-Matin"
-output_path = '/Users/francoislenne/conseil_regional_hdf/audio'
-download_webtv_audio(url, output_path)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Téléchargeur audio WebTV')
+    parser.add_argument('url', type=str, help='URL de la vidéo WebTV')
+    parser.add_argument('output_path', type=str, help='Chemin de sortie pour l\'audio téléchargé')
+
+    args = parser.parse_args()
+
+    downloader = WebTVAudioDownloader(args.url, args.output_path)
+    downloader.download_webtv_audio()
--- src/transform-audio/whisper-audio.py
+++ src/transform-audio/whisper-audio.py
@@ -1,18 +1,43 @@
 import whisper
+import argparse
 from whisper.utils import get_writer
+import torch
+import warnings
 
-# Load the base model
+
+# Ignorer les avertissements
+warnings.filterwarnings("ignore")
+
+
+# Charger le modèle Whisper (peut être 'base', 'small', 'medium', 'large')
 model = whisper.load_model('base')
 
-def get_transcribe(audio: str, language: str = 'fr'):
-    return model.transcribe(audio=audio, language=language, verbose=True)
+print('Modèle chargé avec succès !')
 
-def save_file(results, format: str = 'txt'):
-    writer = get_writer(format, './texte/')
-    writer(results, f'transcribe.{format}')
+class TranscribeResponse:
+    def __init__(self, audio_path: str, text: str):
+        self.audio_path = audio_path
+        self.text = text
+
+    def get_transcribe(audio: str, language: str = 'fr'):
+        print(f'Transcription de {audio} en cours...')
+        return model.transcribe(audio=audio, language=language, verbose=True)
+
+    def save_file(results, format: str = 'txt'):
+        writer = get_writer(format, './texte/')
+        writer(results, f'transcribe.{format}')
 
 if __name__ == "__main__":
-    result = get_transcribe(audio='/Users/francoislenne/conseil_regional_hdf/audio/Séance plénière du 1er février 2024 - Matin – WebTV Hauts-de-France.mp3')
+    parser = argparse.ArgumentParser(description='Transcrire un fichier audio en texte.')
+    parser.add_argument('audio_path', type=str, help='Chemin du fichier audio à transcrire')
+    parser.add_argument('--language', type=str, default='fr', help='Langue de la transcription (par défaut: fr)')
+    parser.add_argument('--format', type=str, default='txt', help='Format de sortie du fichier (par défaut: txt)')
+
+    args = parser.parse_args()
+
+    torch.set_default_dtype(torch.float32)
+
+    result = TranscribeResponse.get_transcribe(audio=args.audio_path, language=args.language)
     print('-'*50)
     print(result.get('text', ''))
-    save_file(result)
+    TranscribeResponse.save_file(result, format=args.format)
